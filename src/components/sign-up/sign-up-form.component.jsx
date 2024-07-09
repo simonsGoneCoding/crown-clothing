10c9da83eaf8ec2039da572b4c2fea9bc648180f
Szymon Warszawa <szymonwarszawa@gmail.com>
2024-07-09 09:40:28 +0100

    Added neew Button component and styles.

--- src/components/sign-up/sign-up-form.component.jsx
+++ src/components/sign-up/sign-up-form.component.jsx
@@ -6,6 +6,8 @@ import {
 } from '../../utils/firebase/firebase.utils';
 
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
   displayName: '',
@@ -53,8 +55,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sing in with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sing in with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
@@ -91,7 +94,7 @@ const SignUpForm = () => {
           onChange={handleChange}
           value={confirmPassword}
         />
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
