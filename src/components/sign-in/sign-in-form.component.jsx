607cdfc34b7412d93ebf75d204ff0a3bc782c1a3
Szymon Warszawa <szymonwarszawa@gmail.com>
2024-07-13 17:34:32 +0100

    Created authentication component alongside sign-in form.

--- src/components/sign-in/sign-in-form.component.jsx
+++ src/components/sign-in/sign-in-form.component.jsx
@@ -3,22 +3,27 @@ import { useState } from 'react';
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
+  signInWithGooglePopup,
+  signInAuthUserWithEmailAndPassword,
 } from '../../utils/firebase/firebase.utils';
 
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
-import './sign-up-form.styles.scss';
+import './sign-in-form.styles.scss';
 
 const defaultFormFields = {
-  displayName: '',
   email: '',
   password: '',
-  confirmPassword: '',
 };
 
-const SignUpForm = () => {
+const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
 
   const handleChange = event => {
     const { name, value } = event.target;
@@ -32,42 +37,36 @@ const SignUpForm = () => {
   const handleSubmit = async event => {
     event.preventDefault();
 
-    if (password !== confirmPassword) {
-      alert('passwords do not match!');
-      return;
-    }
-
     try {
-      const { user } = await createAuthUserWithEmailAndPassword(
+      //LOGIC new logic here
+      const response = await signInAuthUserWithEmailAndPassword(
         email,
         password
       );
-      await createUserDocumentFromAuth(user, { displayName });
+      console.log(response); // REMOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       resetFormFields();
     } catch (error) {
-      // throw error;
-      if (error.code === 'auth/email-already-in-use') {
-        alert('email already in use');
-      } else {
-        throw error;
+      console.log('code: ', error.code); // REMOVE
+      console.log(error); // REMOVE
+
+      switch (error.code) {
+        case 'auth/invalid-credential':
+          alert(
+            'invalid credential, user does not exist or password incorrect'
+          );
+          break;
+        // LOGIC add more eedge cases errors
+        default:
+          console.log(error);
       }
     }
   };
 
   return (
     <div className="sign-up-container">
-      <h2>Don't have an account?</h2>
-      <span>Sing up with your email and password</span>
+      <h2>Already have an account?</h2>
+      <span>Sing in with your email and password</span>
       <form onSubmit={handleSubmit}>
-        <FormInput
-          label="Display Name"
-          type="text"
-          required
-          name="displayName"
-          onChange={handleChange}
-          value={displayName}
-        />
-
         <FormInput
           label="Email"
           required
@@ -85,19 +84,15 @@ const SignUpForm = () => {
           onChange={handleChange}
           value={password}
         />
-
-        <FormInput
-          label="Confirm Password"
-          required
-          type="password"
-          name="confirmPassword"
-          onChange={handleChange}
-          value={confirmPassword}
-        />
-        <Button type="submit">Sign Up</Button>
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button buttonType="google" onClick={signInWithGoogle}>
+            Google Sign In
+          </Button>
+        </div>
       </form>
     </div>
   );
 };
 
-export default SignUpForm;
+export default SignInForm;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -12,10 +12,10 @@ const Navigation = () => {
           <CrownLogo className="logo" />
         </Link>
         <div className="nav-links-container">
-          <Link className="nav-link" to="shop">
+          <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className="nav-link" to="sign-in">
+          <Link className="nav-link" to="/auth">
             SIGN IN
           </Link>
         </div>
